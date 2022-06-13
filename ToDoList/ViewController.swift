3a929d54d22186cdedea6d173f7f6ab8d4e54d7e
Grace Huang <grace_j_huang@outlook.com>
2022-06-13 14:32:13 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Grace Huang on 6/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
